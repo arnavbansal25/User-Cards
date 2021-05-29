df856c95f6f679716477e1f2c3c79170b83ccfd3
Arnav Bansal <arnavbansal.8232@gmail.com>
2021-05-29 12:55:28 +0530

    code optimized

--- user-cards-app/src/App.js
+++ user-cards-app/src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './components/MainComponent';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-
 
 function App() {
   return (
-    <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+    <div className="App">
+      <Main />
+    </div>
   );
 }
 
